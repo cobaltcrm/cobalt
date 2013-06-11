14aa6d759aa6f54b2800abc0b91dc31dbe5caa88
Pablo Larrá <larrapablo@gmail.com>
2013-06-11 13:03:14 -0300

    Update html.php
    I replaced the function "function_exists" to "method_exists" in the line 29.

--- libraries/crm/view/reports/html.php
+++ libraries/crm/view/reports/html.php
@@ -26,7 +26,8 @@ class CobaltViewReportsHtml extends JViewHtml
         //determine view layout
         $this->layout = $this->getLayout();
         $func = "_display_".$this->layout;
-        if ( function_exists($this->$func) ){
+        //if ( function_exists($this->$func) ){ //Don´t work - 2013/06/11 - larrapablo@gmail.com
+        if ( method_exists($this, $func) ){
             $this->$func();
         }
         
