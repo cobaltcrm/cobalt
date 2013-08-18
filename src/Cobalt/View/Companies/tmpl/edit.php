971f294827b7d07cd0e0958e62a2cd6480414c83
David Hurley <david@sparkbuilt.com>
2013-08-18 11:45:25 -0500

    Updating company edit layout (needs category definition added)

--- src/Cobalt/View/Companies/tmpl/edit.php
+++ src/Cobalt/View/Companies/tmpl/edit.php
@@ -9,89 +9,166 @@
 -------------------------------------------------------------------------*/
 // no direct access
 defined( '_CEXEC' ) or die( 'Restricted access' );
+
 $app = JFactory::getApplication();
-$company = $this->company;
-?>
-<form id="edit_form" method="post" action="<?php echo 'index.php?view=companies&task=save'; ?>" onsubmit="return save(this)" >
+
+$format = $app->input->get('format');
+if ($format != "raw")
+{ ?>
+    <h1><?php echo $header; ?></h1>
+    
+<?php } ?>
+
+<form id="edit_form" method="POST" action="<?php echo 'index.php?task=companies.save'; ?>" onsubmit="return save(this)">
+    <?php
+    if ($this->company['id'] != -1)
+    {
+        echo '<input type="hidden" name="id" value="' . $this->company['id'] . '" />';
+    }
+    ?>
+    <div id="editForm">
 
     <ul class="nav nav-tabs" id="myTab">
-      <li class="active"><a href="#Company" data-toggle="tab" >Company</a></li>
-      <li><a href="#Address" data-toggle="tab" >Address</a></li>
-      <li><a href="#Details" data-toggle="tab">Details</a></li>
-      <li><a href="#Custom" data-toggle="tab" >Custom</a></li>
+      <li class="active"><a href="#Company" data-toggle="tab" ><?php echo ucwords(TextHelper::_('COBALT_COMPANY')); ?></a></li>
+      <li><a href="#Address" data-toggle="tab"><?php echo ucwords(TextHelper::_('COBALT_COMPANY_ADDRESS')); ?></a></li>
+      <li><a href="#Assignment" data-toggle="tab"><?php echo ucwords(TextHelper::_('COBALT_COMPANY_ASSIGNMENT')); ?></a></li>
+      <li><a href="#Details" data-toggle="tab"><?php echo ucwords(TextHelper::_('COBALT_DETAILS')); ?></a></li>
+      <li><a href="#Custom" data-toggle="tab"><?php echo ucwords(TextHelper::_('COBALT_CUSTOM')); ?></a></li>
     </ul>
-
+ 
     <div class="tab-content">
       <div class="tab-pane active fade in" id="Company">
-            <div class="cobaltRow">
-            <div class="cobaltField"><?php echo ucwords(TextHelper::_('COBALT_COMPANY_NAME')); ?><span class="required">*</span></div>
-            <div class="cobaltValue"><input class="required inputbox" type="text" name="name" placeholder="<?php echo ucwords(TextHelper::_('COBALT_COMPANY_NAME_NULL')); ?>" value="<?php if(array_key_exists('name',$company)) echo $company['name']; ?>"/></div>
+        <div class="row-fluid">
+            <div class="span6">
+            <label><strong><?php echo ucwords(TextHelper::_('COBALT_COMPANY_NAME')); ?><span class="required">*</span></strong></label>
+            <input type="text" onblur="checkCompanyName(this);" class="required" name="name" id="company_name" placeholder="<?php echo ucwords(TextHelper::_('COBALT_COMPANY_NAME_NULL')); ?>" value="<?php if (array_key_exists('name', $this->company))
+                {
+                    echo $this->company['name'];
+                } ?>"/>
+                <input type="hidden" name="company_id" id="company_id" value=""/>
+                <div id="company_message"></div>
+            </div>
+            <div class="span6">
+                <label><strong><?php echo ucwords(TextHelper::_('COBALT_COMPANY_CATEGORY')); ?></strong></label>
+                <?php echo CompanyHelper::getCategoryDropdown($this->company['category_id'],'category_id','span6'); ?>
+            </div>
         </div>
-        <div class="cobaltRow">
-            <div class="cobaltField"><?php echo ucwords(TextHelper::_('COBALT_COMPANY_PHONE')); ?></div>
-            <div class="cobaltValue"><input class="inputbox" type="text" name="phone" placeholder="<?php echo ucwords(TextHelper::_('COBALT_COMPANY_PHONE_NULL')); ?>" value="<?php if(array_key_exists('phone',$company)) echo $company['phone']; ?>"/></div>
+        <div class="row-fluid">
+            <div class="span6">
+            <label><strong><?php echo ucwords(TextHelper::_('COBALT_COMPANY_PHONE')); ?></strong></label>
+            <input type="text" name="phone" placeholder="<?php echo ucwords(TextHelper::_('COBALT_COMPANY_PHONE_NULL')); ?>" value="<?php if (array_key_exists('phone', $this->company))
+                {
+                    echo $this->company['phone'];
+                } ?>"/>
+            </div>
+            <div class="span6">
+            <label><strong><?php echo ucwords(TextHelper::_('COBALT_COMPANY_FAX')); ?></strong></label>
+            <input type="text" name="fax" placeholder="<?php echo ucwords(TextHelper::_('COBALT_COMPANY_FAX_NULL')); ?>" value="<?php if (array_key_exists('fax', $this->company))
+                {
+                    echo $this->company['fax'];
+                } ?>"/>
+            </div>
         </div>
-        <div class="cobaltRow">
-            <div class="cobaltField"><?php echo ucwords(TextHelper::_('COBALT_COMPANY_FAX')); ?></div>
-            <div class="cobaltValue"><input class="inputbox" type="text" name="fax" placeholder="<?php echo ucwords(TextHelper::_('COBALT_COMPANY_FAX_NULL')); ?>" value="<?php if(array_key_exists('fax',$company)) echo $company['fax']; ?>"/></div>
+        <div class="row-fluid">
+            <div class="span6">
+            <label><strong><?php echo ucwords(TextHelper::_('COBALT_COMPANY_EMAIL')); ?></strong></label>
+            <input type="text" name="email" placeholder="<?php echo ucwords(TextHelper::_('COBALT_COMPANY_EMAIL_NULL')); ?>" value="<?php if (array_key_exists('email', $this->company))
+                {
+                    echo $this->company['email'];
+                } ?>"/>
+            </div>
+            <div class="span6">
+            <label><strong><?php echo TextHelper::_('COBALT_COMPANY_WEB'); ?></strong></label>
+                <input type="text" placeholder="<?php echo TextHelper::_('COBALT_WEBSITE_NULL'); ?>" name="website" value="<?php if (array_key_exists('website', $this->company))
+                {
+                    echo $this->company['website'];
+                } ?>"/>
+            </div>
         </div>
-        <div class="cobaltRow">
-            <div class="cobaltField"><?php echo ucwords(TextHelper::_('COBALT_COMPANY_EMAIL')); ?></div>
-            <div class="cobaltValue"><input class="inputbox" type="text" name="email" placeholder="<?php echo ucwords(TextHelper::_('COBALT_COMPANY_EMAIL_NULL')); ?>" value="<?php if(array_key_exists('email',$company)) echo $company['email']; ?>"/></div>
-        </div>
-      </div>
-      <div class="tab-pane fade" id="Address">
-            <div class="cobaltRow">
-                <div class="cobaltField"><?php echo ucwords(TextHelper::_('COBALT_COMPANY_ADDRESS')); ?></div>
-                <div class="cobaltValue">
-                    <input class="inputbox address_one" type="text" placeholder="<?php echo TextHelper::_('COBALT_ADDRESS_1_NULL'); ?>" name="address_1" value="<?php if(array_key_exists('address_1',$company)) echo $company['address_1']; ?>" />
-                    <br />
-                    <input class="inputbox address_two" type="text" placeholder="<?php echo TextHelper::_('COBALT_ADDRESS_2_NULL'); ?>" name="address_2" value="<?php if(array_key_exists('address_2',$company)) echo $company['address_2']; ?>" />
-                    <br />
-                    <input class="inputbox address_city" type="text" placeholder="<?php echo TextHelper::_('COBALT_CITY_NULL'); ?>" name="address_city" value="<?php if(array_key_exists('address_city',$company)) echo $company['address_city']; ?>" />
-                    <input class="inputbox address_state" type="text" placeholder="<?php echo TextHelper::_('COBALT_STATE_NULL'); ?>" name="address_state" value="<?php if(array_key_exists('address_state',$company)) echo $company['address_state']; ?>" />
-                    <input class="inputbox address_zip" type="text" placeholder="<?php echo TextHelper::_('COBALT_ZIP_NULL'); ?>" name="address_zip" value="<?php if(array_key_exists('address_zip',$company)) echo $company['address_zip']; ?>" />
-                    <br />
-                    <input class="inputbox address_country" type="text" placeholder="<?php echo TextHelper::_('COBALT_COUNTRY_NULL'); ?>" name="address_country" value="<?php if(array_key_exists('address_country',$company)) echo $company['address_country']; ?>" />
-                </div>
-            </div>
-      </div>
-      <div class="tab-pane fade" id="Details">
-            <div class="cobaltRow">
-                <div class="cobaltField"><?php echo TextHelper::_('COBALT_COMPANY_WEB'); ?></div>
-                <div class="cobaltValue"><input class="inputbox" type="text" placeholder="<?php echo TextHelper::_('COBALT_WEBSITE_NULL'); ?>" name="website" value="<?php if(array_key_exists('website',$company)) echo $company['website']; ?>" /></div>
-            </div>
-            <div class="cobaltRow">
-                <div class="cobaltField"><?php echo TextHelper::_('COBALT_COMPANY_DESCRIPTION'); ?></div>
-                <div class="cobaltValue">
-                    <textarea class="inputbox" name="description"><?php if(array_key_exists('description',$company)) echo $company['description']; ?></textarea>
-                </div>
-            </div>
-            <div class="cobaltRow" id="other_button">
-                <div class="cobaltField"><?php echo TextHelper::_('COBALT_FACEBOOK_URL'); ?></div>
-                <div class="cobaltValue"><input class="inputbox" type="text" name="facebook_url" value="<?php if(array_key_exists('facebook_url',$company)) echo $company['facebook_url']; ?>" /></div>
-            </div>
-            <div class="cobaltRow" id="other_button">
-                <div class="cobaltField"><?php echo TextHelper::_('COBALT_TWITTER_USER'); ?></div>
-                <div class="cobaltValue"><input class="inputbox" data-minlength="4" type="text" name="twitter_user" value="<?php if(array_key_exists('twitter_user',$company)) echo $company['twitter_user']; ?>" /></div>
-            </div>
-            <div class="cobaltRow" id="other_button">
-                <div class="cobaltField"><?php echo TextHelper::_('COBALT_FLICKR_URL'); ?></div>
-                <div class="cobaltValue"><input class="inputbox" type="text" name="flickr_url" value="<?php if(array_key_exists('flickr_url',$company)) echo $company['flickr_url']; ?>" /></div>
-            </div>
-            <div class="cobaltRow" id="other_button">
-                <div class="cobaltField"><?php echo TextHelper::_('COBALT_YOUTUBE_URL'); ?></div>
-                <div class="cobaltValue"><input class="inputbox" type="text" name="youtube_url" value="<?php if(array_key_exists('youtube_url',$company)) echo $company['youtube_url']; ?>" /></div>
-            </div>
-      </div>
-      <div class="tab-pan fade" id="Custom">
-              <?php echo $this->edit_custom_fields_view->render(); ?>
-      </div>
     </div>
+    <div class="tab-pane fade in" id="Address">
+        <div class="row-fluid" id="address_button">
+            <label><strong><?php echo ucwords(TextHelper::_('COBALT_COMPANY_ADDRESS')); ?></strong></label>
+        </div>
+        <div id="address_info">
+            <div class="row-fluid">
+                    <input class="inputbox address_one" type="text" placeholder="<?php echo TextHelper::_('COBALT_ADDRESS_1_NULL'); ?>" name="address_1" value="<?php if (array_key_exists('address_1', $this->company))
+                    {
+                        echo $this->company['address_1'];
+                    } ?>"/>
+                    <br/>
+                    <input class="inputbox address_two" type="text" placeholder="<?php echo TextHelper::_('COBALT_ADDRESS_2_NULL'); ?>" name="address_2" value="<?php if (array_key_exists('address_2', $this->company))
+                    {
+                        echo $this->company['address_2'];
+                    } ?>"/>
+                    <br/>
+                    <input class="inputbox address_city" type="text" placeholder="<?php echo TextHelper::_('COBALT_CITY_NULL'); ?>" name="address_city" value="<?php if (array_key_exists('address_city', $this->company))
+                    {
+                        echo $this->company['address_city'];
+                    } ?>"/>
+                    <input class="inputbox address_state" type="text" placeholder="<?php echo TextHelper::_('COBALT_STATE_NULL'); ?>" name="address_state" value="<?php if (array_key_exists('address_state', $this->company))
+                    {
+                        echo $this->company['address_state'];
+                    } ?>"/>
+                    <input class="inputbox address_zip" type="text" placeholder="<?php echo TextHelper::_('COBALT_ZIP_NULL'); ?>" name="address_zip" value="<?php if (array_key_exists('address_zip', $this->company))
+                    {
+                        echo $this->company['address_zip'];
+                    } ?>"/>
+                    <br/>
+                    <input class="inputbox address_country" type="text" placeholder="<?php echo TextHelper::_('COBALT_COUNTRY_NULL'); ?>" name="address_country" value="<?php if (array_key_exists('address_country', $this->company))
+                    {
+                        echo $this->company['address_country'];
+                    } ?>"/>
+            </div>
+        </div>
+    </div>
+    <div class="tab-pane fade in" id="Assignment">
+        <?php if (UsersHelper::getRole() == 'exec' || UsersHelper::getRole() == "manager" || !($this->company['id'] > 0) || (array_key_exists('owner_id', $this->company) && UsersHelper::getUserId() == $this->company['owner_id']) || UsersHelper::isAdmin())
+        { ?>
+            <div class="row-fluid">
+                <label><strong><?php echo ucwords(TextHelper::_('COBALT_COMPANY_OWNER')); ?></strong></label>
+                    <?php $ownerId = array_key_exists('owner_id', $this->company) ? $this->company['owner_id'] : UsersHelper::getUserId(); ?>
+                    <?php echo DropdownHelper::getOwnerDropdown($ownerId); ?>
+            </div>
+        <?php } ?>
+        <div class="row-fluid">
+            <label><strong><?php echo TextHelper::_('COBALT_COMPANY_DESCRIPTION'); ?></strong></label>
+                <textarea name="description"><?php if (array_key_exists('description', $this->company))
+                    {
+                        echo $this->company['description'];
+                    } ?></textarea>
+        </div>
+    </div>
+    <div class="tab-pane fade in" id="Details">
+        <div class="row-fluid">
+            <label><strong><?php echo TextHelper::_('COBALT_SOCIAL_MEDIA'); ?></strong></label>
+                <input class="span6" type="text" name="facebook_url" value="<?php if (array_key_exists('facebook_url', $this->company))
+                {
+                    echo $this->company['facebook_url'];
+                } ?>" placeholder="<?php echo TextHelper::_('COBALT_FACEBOOK_URL'); ?>" />
+                <input class="span6" data-minlength="4" type="text" name="twitter_user" value="<?php if (array_key_exists('twitter_user', $this->company))
+                {
+                    echo $this->company['twitter_user'];
+                } ?>" placeholder="<?php echo TextHelper::_('COBALT_TWITTER_USER'); ?>" />
+        </div>
+        <div class="row-fluid">
+                <input class="span6" type="text" name="flickr_url" value="<?php if (array_key_exists('flickr_url', $this->company))
+                {
+                    echo $this->company['flickr_url'];
+                } ?>" placeholder="<?php echo TextHelper::_('COBALT_FLICKR_URL'); ?>" />
+
+                <input class="span6" type="text" name="youtube_url" value="<?php if (array_key_exists('youtube_url', $this->company))
+                {
+                    echo $this->company['youtube_url'];
+                } ?>" placeholder="<?php echo TextHelper::_('COBALT_YOUTUBE_URL'); ?>" />
+        </div>
+    </div>
+    <div class="tab-pane fade in" id="Custom">
+        <?php if ($format != "raw")
+        { ?>
+            <?php echo $this->edit_custom_fields_view->display(); ?>
         <?php
-            if ($company['id'] != -1) {
-                echo '<input type="hidden" name="id" value="'.$company['id'].'" />';
-            }
-        ?>
-    <input type="hidden" name="model" value="company" />
+        } ?>
+    </div>
+    </div>
 </form>
