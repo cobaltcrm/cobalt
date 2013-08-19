3b71292b6f286a84adfe49434ab74b7165fda069
David Hurley <david@sparkbuilt.com>
2013-08-18 19:15:33 -0500

    Updated add goals page

--- src/Cobalt/View/Goals/tmpl/add.php
+++ src/Cobalt/View/Goals/tmpl/add.php
@@ -10,33 +10,37 @@
 // no direct access
 defined( '_CEXEC' ) or die( 'Restricted access' ); ?>
 
-<h1><?php echo ucwords(TextHelper::_('COBALT_GOALS_HEADER')); ?></h1>
+<div class="page-header">
+    <h1><?php echo ucwords(TextHelper::_('COBALT_GOALS_HEADER')); ?></h1>
+</div>
 <div class="goals_columns row">
     <div class="goals_left_column span6">
         <ul class="unstyled">
-            <li class="alert alert-success">
+            <li>
                 <div class="goal_container media">
                     <div class="goal_img pull-left"><img src="<?php echo JURI::base(); ?>src/Cobalt/media/images/win_more_cash.png" /></div>
                     <div class="goal_info_container media-body">
-                        <h2><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=win_cash'); ?>"><?php echo ucwords(TextHelper::_('COBALT_WIN_MORE_CASH').' '.ConfigHelper::getConfigValue('currency')); ?></a></h2>
+                        <h4><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=win_cash'); ?>"><?php echo ucwords(TextHelper::_('COBALT_WIN_MORE_CASH')); ?></a></h4>
                         <div class="goal_info"><?php echo JText::sprintf('COBALT_CREATE_GOAL_TRACK_CASH',ConfigHelper::getConfigValue('currency')); ?></div>
                     </div>
                 </div>
+                <hr />
             </li>
-            <li class="alert">
+            <li>
                 <div class="goal_container media">
                     <div class="goal_img pull-left"><img src="<?php echo JURI::base(); ?>src/Cobalt/media/images/win_more_deals.png" /></div>
                     <div class="goal_info_container media-body">
-                        <h2><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=win_deals'); ?>"><?php echo ucwords(TextHelper::_('COBALT_WIN_MORE_DEALS')); ?></a></h2>
+                        <h4><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=win_deals'); ?>"><?php echo ucwords(TextHelper::_('COBALT_WIN_MORE_DEALS')); ?></a></h4>
                         <div class="goal_info"><?php echo TextHelper::_('COBALT_CREATE_GOAL_TRACK_DEALS'); ?></div>
                     </div>
                 </div>
+                <hr />
             </li>
-            <li class="alert alert-info">
+            <li>
                 <div class="goal_container media">
                     <div class="goal_img pull-left"><img src="<?php echo JURI::base(); ?>src/Cobalt/media/images/move_deals_forward.png" /></div>
                     <div class="goal_info_container media-body">
-                        <h2><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=move_deals'); ?>"><?php echo ucwords(TextHelper::_('COBALT_MOVE_DEALS_FORWARD')); ?></a></h2>
+                        <h4><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=move_deals'); ?>"><?php echo ucwords(TextHelper::_('COBALT_MOVE_DEALS_FORWARD')); ?></a></h4>
                         <div class="goal_info"><?php echo TextHelper::_('COBALT_CREATE_GOAL_TRACK_DEAL_STAGES'); ?></div>
                     </div>
                 </div>
@@ -45,29 +49,31 @@ defined( '_CEXEC' ) or die( 'Restricted access' ); ?>
     </div>
     <div class="goals_right_column span6">
         <ul class="unstyled">
-            <li class="alert">
+            <li>
                 <div class="goal_container media">
                     <div class="goal_img pull-left"><img src="<?php echo JURI::base(); ?>src/Cobalt/media/images/complete_more_tasks.png" /></div>
                     <div class="goal_info_container media-body">
-                        <h2><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=complete_tasks'); ?>"><?php echo ucwords(TextHelper::_('COBALT_COMPLETE_TASKS')); ?></a></h2>
+                        <h4><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=complete_tasks'); ?>"><?php echo ucwords(TextHelper::_('COBALT_COMPLETE_TASKS')); ?></a></h4>
                         <div class="goal_info"><?php echo TextHelper::_('COBALT_CREATE_GOAL_TRACK_TASKS'); ?></div>
                     </div>
                 </div>
+                <hr />
             </li>
-            <li class="alert-info alert">
+            <li>
                 <div class="goal_container media">
                     <div class="goal_img pull-left"><img src="<?php echo JURI::base(); ?>src/Cobalt/media/images/write_more_notes.png" /></div>
                     <div class="goal_info_container media-body">
-                        <h2><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=write_notes'); ?>"><?php echo ucwords(TextHelper::_('COBALT_WRITE_NOTES')); ?></a></h2>
+                        <h4><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=write_notes'); ?>"><?php echo ucwords(TextHelper::_('COBALT_WRITE_NOTES')); ?></a></h4>
                         <div class="goal_info"><?php echo TextHelper::_('COBALT_CREATE_GOAL_TRACK_NOTES'); ?></div>
                     </div>
                 </div>
+                <hr />
             </li>
-            <li class="alert alert-success">
+            <li>
                 <div class="goal_container media">
                     <div class="goal_img pull-left"><img src="<?php echo JURI::base(); ?>src/Cobalt/media/images/create_deals.png" /></div>
                     <div class="goal_info_container media-body">
-                        <h2><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=create_deals'); ?>"><?php echo ucwords(TextHelper::_('COBALT_CREATE_DEALS')); ?></a></h2>
+                        <h4><a href="<?php echo JRoute::_('index.php?view=goals&layout=edit&type=create_deals'); ?>"><?php echo ucwords(TextHelper::_('COBALT_CREATE_DEALS')); ?></a></h4>
                         <div class="goal_info"><?php echo TextHelper::_('COBALT_CREATE_GOAL_TRACK_DEALS_CREATED'); ?></div>
                     </div>
                 </div>
