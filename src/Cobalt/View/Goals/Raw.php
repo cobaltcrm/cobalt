209a7ca32d9abe8fcc61197933bbc8167216cc36
David Hurley <david@sparkbuilt.com>
2013-08-18 19:26:11 -0500

    Removed fullscreen from JS, started goal modal add/edit

--- src/Cobalt/View/Goals/Raw.php
+++ src/Cobalt/View/Goals/Raw.php
@@ -72,7 +72,113 @@ class Raw extends AbstractHtmlView
             //assign view references
             $this->goals = $goals;
 
+        } elseif ( $this->getLayout() == 'edit' ) {
+
+            switch ($type) {
+                case "win_cash":
+                    $header = ucwords(TextHelper::_('COBALT_WIN_MORE_CASH'));
+                    break;
+                case "win_deals":
+                    $header = ucwords(TextHelper::_('COBALT_WIN_MORE_DEALS'));
+                    break;
+                case "move_deals";
+                    $header = ucwords(TextHelper::_('COBALT_MOVE_DEALS_FORWARD'));
+                    break;
+                case "complete_tasks";
+                    $header = ucwords(TextHelper::_('COBALT_COMPLETE_TASKS'));
+                    break;
+                case "write_notes";
+                    $header = ucwords(TextHelper::_('COBALT_WRITE_NOTES'));
+                    break;
+                case "create_deals";
+                    $header = ucwords(TextHelper::_('COBALT_CREATE_DEALS'));
+                    break;
+                default:
+                   $app->redirect('index.php?view=goals');
+                    break;
+            }
+
+            $this->header = $header;
+
+        } elseif ( $this->getLayout() != 'add' ) {
+
+            //load model
+            $model = new GoalModel;
+
+            //get all goals from model depending on user type
+            $member_role = UsersHelper::getRole();
+
+            //basic members
+            if ($member_role == 'basic') {
+                $individual_goals = $model->getIndividualGoals();
+                $team_goals = $model->getTeamGoals();
+                $company_goals = $model->getCompanyGoals();
+                $leaderboards = $model->getLeaderBoards();
+            }
+
+            //managers
+            if ($member_role == 'manager') {
+                // $individual_goals = $model->getManagerIndividualGoals();
+                $individual_goals = $model->getIndividualGoals();
+                $team_goals = $model->getTeamGoals();
+                $company_goals = $model->getCompanyGoals();
+                $leaderboards = $model->getLeaderBoards();
+            }
+
+            //executives
+            if ($member_role == 'exec') {
+                // $individual_goals = $model->getExecIndividualGoals();
+                $individual_goals = $model->getIndividualGoals();
+                // $team_goals = $model->getExecTeamGoals();
+                $team_goals = $model->getTeamGoals();
+                $company_goals = $model->getCompanyGoals();
+                $leaderboards = $model->getLeaderBoards();
+            }
+
+            //assign goals to global goal object to pass through to view
+            $goals = new \stdClass();
+            $goals->individual_goals = $individual_goals;
+            $goals->team_goals = $team_goals;
+            $goals->company_goals = $company_goals;
+            $goals->leaderboards = $leaderboards;
+
+            //if we get results then load the default goals page else show the add goals page
+            $goal_count = false;
+            foreach ($goals as $goal_list) {
+                if ( count($goal_list) > 0 ) {
+                    $goal_count = true;
+                }
+            }
+            if ($goal_count) {
+                //set layout
+                $this->setLayout('default');
+                //assign view refs
+                $this->goals = $goals;
+            } else {
+                //add goal layout
+                $this->setLayout('add');
+            }
         }
+
+        //load java libs
+        $doc = JFactory::getDocument();
+        $doc->addScript( JURI::base().'src/Cobalt/media/js/goal_manager.js' );
+
+        //get associated members and teams
+        $teams = UsersHelper::getTeams();
+        $users = UsersHelper::getUsers();
+        $member_role = UsersHelper::getRole();
+        $user_id = UsersHelper::getUserId();
+        $team_id = UsersHelper::getTeamId();
+
+        //assign view refs
+        $this->type = $type;
+        $this->teams = $teams;
+        $this->users = $users;
+        $this->user_id = $user_id;
+        $this->team_id = $team_id;
+        $this->member_role = $member_role;
+        $this->leaderboard_list = DropdownHelper::getLeaderBoards();
 
         //display
         echo parent::render();
--- themes/bootstrap/js/cobalt.js
+++ themes/bootstrap/js/cobalt.js
@@ -33,27 +33,6 @@ var Cobalt = {
         });
     },
 
-    toggleFullScreen: function() {
-        if ((document.fullScreenElement && document.fullScreenElement !== null) ||    // alternative standard method
-            (!document.mozFullScreenElement && !document.webkitFullScreenElement)) {  // current working methods
-            if (document.documentElement.requestFullScreen) {
-                document.documentElement.requestFullScreen();
-            } else if (document.documentElement.mozRequestFullScreen) {
-                document.documentElement.mozRequestFullScreen();
-            } else if (document.documentElement.webkitRequestFullScreen) {
-                document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-            }
-        } else {
-            if (document.cancelFullScreen) {
-                document.cancelFullScreen();
-            } else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            } else if (document.webkitCancelFullScreen) {
-                document.webkitCancelFullScreen();
-            }
-        }
-    },
-
     saveItem: function (formId) {
         var dataObj = {},
             type = null,
