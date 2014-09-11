5015301818d0da23c5ec1de6f28f1ee244527145
Jan Linhart <admin@escope.cz>
2014-09-11 09:36:10 +0200

    SortTable JS function call is not needed when dataTable is taking care of sorting a table

--- src/Cobalt/View/Deals/tmpl/list.php
+++ src/Cobalt/View/Deals/tmpl/list.php
@@ -14,14 +14,14 @@ $app = JFactory::getApplication();
 <thead>
     <tr>
         <th class="checkbox_column"><input rel="tooltip" title="<?php echo TextHelper::_('COBALT_CHECK_ALL_ITEMS'); ?>" data-placement="bottom" type="checkbox" onclick="selectAll(this);" /></th>
-        <th class="name" ><div class="sort_order"><a href="javascript:void(0);" class="d.name" onclick="sortTable('d.name',this)"><?php echo ucwords(TextHelper::_('COBALT_DEALS_NAME')); ?></a></div></th>
-        <th class="company"><div class="sort_order"><a href="javascript:void(0);" class="c.name" onclick="sortTable('c.name',this)"><?php echo ucwords(TextHelper::_('COBALT_DEALS_COMPANY')); ?></a></div></th>
-        <th class="amount" ><div class="sort_order"><a href="javascript:void(0);" class="d.amount" onclick="sortTable('d.amount',this)"><?php echo ucwords(TextHelper::_('COBALT_DEALS_AMOUNT')); ?></a></div></th>
-        <th class="status" ><div class="sort_order"><a href="javascript:void(0);" class="d.status_id" onclick="sortTable('d.status_id',this)"><?php echo ucwords(TextHelper::_('COBALT_DEALS_STATUS')); ?></a></div></th>
-        <th class="stage" ><div class="sort_order"><a href="javascript:void(0);" class="d.stage_id" onclick="sortTable('d.stage_id',this)"><?php echo ucwords(TextHelper::_('COBALT_DEALS_STAGE')); ?></a></div></th>
-        <th class="source" ><div class="sort_order"><a href="javascript:void(0);" class="d.source_id" onclick="sortTable('d.source_id',this)"><?php echo ucwords(TextHelper::_('COBALT_DEAL_SOURCE')); ?></a></div></th>
-        <th class="expected_close" ><div class="sort_order"><a href="javascript:void(0);" class="d.expected_close" onclick="sortTable('d.expected_close',this)"><?php echo ucwords(TextHelper::_('COBALT_DEALS_EXPECTED_CLOSE')); ?></a></div></th>
-        <th class="actual_close" ><div class="sort_order"><a href="javascript:void(0);" class="d.actual_close" onclick="sortTable('d.actual_close',this)"><?php echo ucwords(TextHelper::_('COBALT_DEALS_ACTUAL_CLOSE')); ?></a></div></th>
+        <th class="name" ><div class="sort_order"><a href="javascript:void(0);" class="d.name"><?php echo ucwords(TextHelper::_('COBALT_DEALS_NAME')); ?></a></div></th>
+        <th class="company"><div class="sort_order"><a href="javascript:void(0);" class="c.name"><?php echo ucwords(TextHelper::_('COBALT_DEALS_COMPANY')); ?></a></div></th>
+        <th class="amount" ><div class="sort_order"><a href="javascript:void(0);" class="d.amount"><?php echo ucwords(TextHelper::_('COBALT_DEALS_AMOUNT')); ?></a></div></th>
+        <th class="status" ><div class="sort_order"><a href="javascript:void(0);" class="d.status_id"><?php echo ucwords(TextHelper::_('COBALT_DEALS_STATUS')); ?></a></div></th>
+        <th class="stage" ><div class="sort_order"><a href="javascript:void(0);" class="d.stage_id"><?php echo ucwords(TextHelper::_('COBALT_DEALS_STAGE')); ?></a></div></th>
+        <th class="source" ><div class="sort_order"><a href="javascript:void(0);" class="d.source_id"><?php echo ucwords(TextHelper::_('COBALT_DEAL_SOURCE')); ?></a></div></th>
+        <th class="expected_close" ><div class="sort_order"><a href="javascript:void(0);" class="d.expected_close"><?php echo ucwords(TextHelper::_('COBALT_DEALS_EXPECTED_CLOSE')); ?></a></div></th>
+        <th class="actual_close" ><div class="sort_order"><a href="javascript:void(0);" class="d.actual_close"><?php echo ucwords(TextHelper::_('COBALT_DEALS_ACTUAL_CLOSE')); ?></a></div></th>
         <th class="contacts" >&nbsp;</th>
     </tr>
 </thead>
