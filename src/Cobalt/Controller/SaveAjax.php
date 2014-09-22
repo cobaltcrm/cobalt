6403d502495ad5d1e94fe090585d76a66ba05295
Júlio Pontes <juliopfneto@gmail.com>
2014-09-22 18:05:09 -0300

    Fixes
    Deal Edit Items not fill date

--- src/Cobalt/Controller/SaveAjax.php
+++ src/Cobalt/Controller/SaveAjax.php
@@ -13,6 +13,8 @@ namespace Cobalt\Controller;
 // no direct access
 defined( '_CEXEC' ) or die( 'Restricted access' );
 
+use Cobalt\Table\AbstractTable;
+
 class SaveAjax extends DefaultController
 {
     public function execute()
@@ -20,13 +22,18 @@ class SaveAjax extends DefaultController
         $item_id = $this->input->get('item_id');
         $item_type = $this->input->get('item_type');
         $field = $this->input->get('field');
-        $value = $this->input->get('value');
+        $value = $this->input->getString('value');
 
         $db = $this->container->fetch('db');
 
         $data = array('id' => $item_id, $field => $db->escape($value));
         $post_data = $_POST;
-
+        foreach (array('item_id','item_type','field', 'value', 'format', 'tmpl') as $key) {
+            if (isset($post_data[$key])) {
+                unset($post_data[$key]);
+            }
+        }
+        $post_data = array_filter($post_data);
         $data = array_merge($data, $post_data);
 
         $modelClass = 'Cobalt\\Model\\' . ucfirst($item_type);
@@ -36,7 +43,11 @@ class SaveAjax extends DefaultController
         $returnRow = true;
         $return = $model->store($data, $returnRow);
 
-        echo json_encode($return);
+        if ($return instanceof AbstractTable) {
+            echo json_encode($return->getProperties());
+        } else {
+            echo json_encode($return);
+        }
     }
 
 }
