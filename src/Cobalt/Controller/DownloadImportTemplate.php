0c2c53bc7ac56cf88aef905b3f3d9ec108ec0d15
Michael Babker <mbabker@flbab.com>
2014-10-01 17:40:01 -0400

    Add a top level media folder, intent is to refactor all uses of src/Cobalt/media to use this instead

--- src/Cobalt/Controller/DownloadImportTemplate.php
+++ src/Cobalt/Controller/DownloadImportTemplate.php
@@ -21,7 +21,7 @@ class DownloadImportTemplate extends DefaultController
     {
         $template_type = $this->input->get('template_type');
 
-        $path = JPATH_SITE . '/src/Cobalt/media/import_templates/import_' . $template_type . '.csv';
+        $path = JPATH_SITE . '/media/import_templates/import_' . $template_type . '.csv';
 
         if (!file_exists($path))
         {
