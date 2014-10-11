8da9f1cf76b6c440b128703926e1f6682f24e50f
Michael Babker <mbabker@flbab.com>
2014-10-11 13:21:10 -0400

    Code style

--- src/Cobalt/Templating/TemplateReference.php
+++ src/Cobalt/Templating/TemplateReference.php
@@ -17,81 +17,124 @@ use Symfony\Component\Templating\TemplateReferenceInterface;
  */
 class TemplateReference implements TemplateReferenceInterface
 {
-    protected $parameters;
+	/**
+	 * Array containing the template params
+	 *
+	 * @var    array
+	 * @since  1.0
+	 */
+	protected $parameters;
 
-    public function __construct($name, $view)
-    {
-        $this->parameters = array(
-            'name' => $name,
-            'view' => $view,
-        );
-    }
+	/**
+	 * Constructor
+	 *
+	 * @param   string  $name  The layout name to render
+	 * @param   string  $view  The view the layout falls under
+	 *
+	 * @since   1.0
+	 */
+	public function __construct($name, $view)
+	{
+		$this->parameters = array(
+			'name' => $name,
+			'view' => $view,
+		);
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function __toString()
-    {
-        return $this->getLogicalName();
-    }
+	/**
+	 * Returns the string representation as shortcut for getLogicalName().
+	 *
+	 * Alias of getLogicalName().
+	 *
+	 * @return  string  The template name
+	 *
+	 * @since   1.0
+	 */
+	public function __toString()
+	{
+		return $this->getLogicalName();
+	}
 
-    /**
-     * {@inheritdoc}
-     *
-     * @api
-     */
-    public function set($name, $value)
-    {
-        if (array_key_exists($name, $this->parameters)) {
-            $this->parameters[$name] = $value;
-        } else {
-            throw new \InvalidArgumentException(sprintf('The template does not support the "%s" parameter.', $name));
-        }
+	/**
+	 * Sets a template parameter.
+	 *
+	 * @param   string  $name  The parameter name
+	 * @param   string  $value The parameter value
+	 *
+	 * @return  $this
+	 *
+	 * @since   1.0
+	 * @throws  \InvalidArgumentException if the parameter name is not supported
+	 */
+	public function set($name, $value)
+	{
+		if (!array_key_exists($name, $this->parameters))
+		{
+			throw new \InvalidArgumentException(sprintf('The template does not support the "%s" parameter.', $name));
+		}
 
-        return $this;
-    }
+		$this->parameters[$name] = $value;
 
-    /**
-     * {@inheritdoc}
-     *
-     * @api
-     */
-    public function get($name)
-    {
-        if (array_key_exists($name, $this->parameters)) {
-            return $this->parameters[$name];
-        }
+		return $this;
+	}
 
-        throw new \InvalidArgumentException(sprintf('The template does not support the "%s" parameter.', $name));
-    }
+	/**
+	 * Gets a template parameter.
+	 *
+	 * @param   string  $name  The parameter name
+	 *
+	 * @return  string  The parameter value
+	 *
+	 * @since   1.0
+	 * @throws  \InvalidArgumentException if the parameter name is not supported
+	 */
+	public function get($name)
+	{
+		if (array_key_exists($name, $this->parameters))
+		{
+			return $this->parameters[$name];
+		}
 
-    /**
-     * {@inheritdoc}
-     *
-     * @api
-     */
-    public function all()
-    {
-        return $this->parameters;
-    }
+		throw new \InvalidArgumentException(sprintf('The template does not support the "%s" parameter.', $name));
+	}
 
-    /**
-     * {@inheritdoc}
-     *
-     * @api
-     */
-    public function getPath()
-    {
-        return $this->parameters['name'];
-    }
+	/**
+	 * Gets the template parameters.
+	 *
+	 * @return  array  An array of parameters
+	 *
+	 * @since   1.0
+	 */
+	public function all()
+	{
+		return $this->parameters;
+	}
 
-    /**
-     * {@inheritdoc}
-     *
-     * @api
-     */
-    public function getLogicalName()
-    {
-        return $this->parameters['name'];
-    }
+	/**
+	 * Returns the path to the template.
+	 *
+	 * By default, it just returns the template name.
+	 *
+	 * @return  string  A path to the template or a resource
+	 *
+	 * @since   1.0
+	 */
+	public function getPath()
+	{
+		return $this->parameters['name'];
+	}
+
+	/**
+	 * Returns the "logical" template name.
+	 *
+	 * The template name acts as a unique identifier for the template.
+	 *
+	 * @return  string  The template name
+	 *
+	 * @since   1.0
+	 */
+	public function getLogicalName()
+	{
+		return $this->parameters['name'];
+	}
 }
